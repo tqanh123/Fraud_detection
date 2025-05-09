612fec927b046a2ef555738ddf5158fccd8b5460
Sleepapp <vontthao@gmail.com>
2025-05-09 16:13:23 +0700

    Xử lí nhánh với main

--- Website/api/views.py
+++ Website/api/views.py
@@ -9,17 +9,162 @@ from django.contrib import messages
 from pyspark.sql import SparkSession
 
 from .models import DataFileUpload
-from .utils import extract_time_features, load_model, run_prediction
+from .utils import extract_time_features, load_model
+from ModelTraining.RandomForest import RandomForestClassifierWrapper
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, to_date, date_format, count, lit
 
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+# $env:SPARK_HOME = "D:/App/spark-3.5.5-bin-hadoop3"
+# $env:HADOOP_HOME = "D:/App/hadoop"
+# $env:JAVA_HOME = "C:/Program Files/Java/jdk-24"
+# $env:PYSPARK_PYTHON = "..\\venv\\Scripts\\python.exe"
+# $env:PYSPARK_DRIVER_PYTHON = "..\\venv\\Scripts\\python.exe"
+
+os.environ["SPARK_HOME"] = "D:/App/spark-3.5.5-bin-hadoop3"
+os.environ["HADOOP_HOME"] = "D:/App/hadoop"
+os.environ["JAVA_HOME"] = "C:/Program Files/Java/jdk-1.8"
+os.environ["PYSPARK_PYTHON"] = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "venv", "Scripts", "python.exe")
+os.environ["PYSPARK_DRIVER_PYTHON"] = os.environ["PYSPARK_PYTHON"]
 
 def base(request):
     return render(request,'api/landing_page.html')
     
 def upload_credit_data(request):
     return render(request,'api/upload_credit_data.html')
+
+def prediction_button(request, id):
+    # Set environment variables
+    os.environ["PYSPARK_PYTHON"] = "..\\venv\\Scripts\\python.exe"
+    os.environ["PYSPARK_DRIVER_PYTHON"] = "..\\venv\\Scripts\\python.exe"
+
+    try:
+        # Load the data file
+        obj = DataFileUpload.objects.get(id=id)
+        spark = SparkSession.builder.appName("FraudDetection").getOrCreate()
+        df = spark.read.csv(obj.actual_file.path, header=True, inferSchema=True)
+
+        # Extract time features (if needed for the model)
+        df = extract_time_features(df)
+
+        # Load the trained model
+        # trained_model = load_model('Website/ModelTraining/LogisticRegressionModel')
+        loaded_pipeline_model, trained_rf_model = RandomForestClassifierWrapper.load_model_pip('Website/ModelTraining/RandomForestModel')
+        # loaded_pipeline_model, trained_xgb_model = XGBoostClassifierWrapper.load_model_pip('Website/ModelTraining/XGBoostModel')
+        df = loaded_pipeline_model.transform(df)
+
+    # Make predictions
+        predictions = trained_rf_model.transform(df)
+        print("Model loaded successfully:", trained_rf_model)
+
+        # Convert predictions to Pandas DataFrame
+        pandas_df = predictions.toPandas()
+
+        # Prepare data for the bar chart
+        # Extract fraud probabilities (x = not fraud, y = fraud)
+        if "probability" in predictions.columns:
+            pandas_df['not_fraud'] = pandas_df['probability'].apply(lambda prob: prob[0] * 100)  # Convert to percentage
+            pandas_df['fraud'] = pandas_df['probability'].apply(lambda prob: prob[1] * 100)  # Convert to percentage
+
+            # Group by CUSTOMER_ID and calculate average probabilities
+            bar_data = pandas_df.groupby("CUSTOMER_ID")[["not_fraud", "fraud"]].mean().reset_index()
+
+            # Prepare data for the bar chart
+            bar_chart = {
+                "categories": bar_data["CUSTOMER_ID"].tolist(),
+                "series": [
+                    {"name": "Not Fraud", "data": bar_data["not_fraud"].tolist()},
+                    {"name": "Fraud", "data": bar_data["fraud"].tolist()}
+                ]
+            }
+        else:
+            bar_chart = {"categories": [], "series": []}
+
+        print("Bar chart data:", bar_chart)
+
+        # Drop unnecessary columns
+        columns_to_drop = ['features', 'scaled_features', 'rawPrediction', 'probability', 'not_fraud', 'fraud']
+        pandas_df = pandas_df.drop(columns=columns_to_drop, errors='ignore')
+
+
+        # Pass data to the template
+        return render(request, 'api/fraud_detection.html', {
+            'id': id,
+            'table_data': pandas_df.to_dict(orient="records"),
+            'columns': pandas_df.columns.tolist(),
+            'bar_chart': bar_chart
+        })
+
+    except FileNotFoundError as e:
+        return HttpResponse(f"Error: {str(e)}", status=404)
+    except RuntimeError as e:
+        return HttpResponse(f"Error: {str(e)}", status=500)
+    except Exception as e:
+        return HttpResponse(f"An unexpected error occurred: {str(e)}", status=500)
+
+# def prediction_button(request, id):
+#     # Set environment variables
+#     os.environ["PYSPARK_PYTHON"] = "..\\venv\\Scripts\\python.exe"
+#     os.environ["PYSPARK_DRIVER_PYTHON"] = "..\\venv\\Scripts\\python.exe"
+
+#     try:
+#         # Load the data file
+#         obj = DataFileUpload.objects.get(id=id)
+#         spark = SparkSession.builder.appName("FraudDetection").getOrCreate()
+#         df = spark.read.csv(obj.actual_file.path, header=True, inferSchema=True)
+
+#         # Extract time features (if needed for the model)
+#         df = extract_time_features(df)
+
+#         # Load the trained model
+#         trained_model = load_model('ModelTraining/LogisticRegressionModel')
+
+#         # Make predictions
+#         predictions = trained_model.transform(df)
+#         print("Model loaded successfully:", trained_model)
+
+#         # Convert predictions to Pandas DataFrame
+#         pandas_df = predictions.toPandas()
+
+#         # Drop unnecessary columns
+#         columns_to_drop = ['features', 'scaled_features', 'rawPrediction', 'probability']
+#         pandas_df = pandas_df.drop(columns=columns_to_drop, errors='ignore')
+
+#         # Prepare data for the bar chart
+#         if "probability" in predictions.columns:
+#             # Extract fraud probability (y value from [x, y])
+#             pandas_df['fraud_probability'] = predictions.select("probability").rdd.map(lambda row: row.probability[1]).collect()
+
+#             # Group by CUSTOMER_ID and calculate average fraud probability
+#             bar_data = pandas_df.groupby("CUSTOMER_ID")['fraud_probability'].mean().reset_index()
+
+#             bar_chart = {
+#                 "categories": bar_data["CUSTOMER_ID"].tolist(),
+#                 "series": [{"name": "Fraud Probability", "data": bar_data["fraud_probability"].tolist()}]
+#             }
+#         else:
+#             bar_chart = {"categories": [], "series": []}
+
+#         # Pass data to the template
+#         return render(request, 'api/fraud_detection.html', {
+#             'id': id,
+#             'table_data': pandas_df.to_dict(orient="records"),
+#             'columns': pandas_df.columns.tolist(),
+#             'bar_chart': bar_chart
+#         })
+
+#     except FileNotFoundError as e:
+#         return HttpResponse(f"Error: {str(e)}", status=404)
+#     except RuntimeError as e:
+#         return HttpResponse(f"Error: {str(e)}", status=500)
+#     except Exception as e:
+#         return HttpResponse(f"An unexpected error occurred: {str(e)}", status=500)
+
+
 def reports(request):
     all_data_files_objs=DataFileUpload.objects.all()
     return render(request,'api/reports.html',{'all_files':all_data_files_objs})
@@ -50,72 +195,93 @@ def upload_data(request):
                 messages.warning(request, "Invalid/wrong format. Please upload File.")
                 return redirect('/upload_credit_data')
             
-def view_data(request, id):
+def view_data(request,id):
     obj = DataFileUpload.objects.get(id=id)
     df = pd.read_csv(obj.actual_file.path)
     columns = df.columns.tolist()
-    data = df.to_dict(orient='records')  # Convert rows to list of dicts
-    return render(request, 'api/view_data.html', {
-        'id': id,
-        'columns': columns,
-        'data': data
-    })
+    return render(request,'api/view_data.html', {'id': id, 'columns': columns})
 
 def retrieve_data_by_id(request, id):
+    try:
+        os.environ["PYSPARK_PYTHON"] = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "venv", "Scripts", "python.exe")
+        os.environ["PYSPARK_DRIVER_PYTHON"] = os.environ["PYSPARK_PYTHON"]
 
-    # Initialize PySpark session
-    spark = SparkSession.builder.appName("FraudDetection").getOrCreate()
+        logger.info("Checking Spark session status...")
+        if SparkSession._instantiatedSession is None:
+            logger.info("Spark session has NOT been initialized.")
+        else:
+            logger.info("Spark session is already initialized.")
 
-    # Load the data
-    obj = DataFileUpload.objects.get(id=id)
-    df = spark.read.csv(obj.actual_file.path, header=True, inferSchema=True)
+        # Thêm thông tin về đường dẫn
+        logger.info(f"SPARK_HOME: {os.environ.get('SPARK_HOME')}")
+        logger.info(f"HADOOP_HOME: {os.environ.get('HADOOP_HOME')}")
+        logger.info(f"JAVA_HOME: {os.environ.get('JAVA_HOME')}")
+        logger.info(f"PYSPARK_PYTHON: {os.environ.get('PYSPARK_PYTHON')}")
 
-    # Receive parameters from the frontend
-    customer_id = request.GET.get('customer_id')
-    tx_month = request.GET.get('tx_month')
 
-    # Convert TX_DATETIME to date and extract MM-YYYY
-    df = df.withColumn("TX_DATETIME", to_date(col("TX_DATETIME"), "yyyy-MM-dd HH:mm:ss"))
-    df = df.withColumn("TX_MONTH", date_format(col("TX_DATETIME"), "MM-yyyy"))
+        # Khởi tạo Spark session
+        spark = SparkSession.builder.appName("FraudDetection").getOrCreate()
+        print("Spark session initialized successfully.")
 
-    # Filter by customer_id and tx_month
-    if customer_id:
-        df = df.filter(col("CUSTOMER_ID") == int(customer_id))
-    if tx_month:
-        df = df.filter(col("TX_MONTH") == tx_month)
+        # Load the data
+        obj = DataFileUpload.objects.get(id=id)
+        df = spark.read.csv(obj.actual_file.path, header=True, inferSchema=True)
 
-    # Convert to Pandas for DataTables
-    pandas_df = df.toPandas()
+        # Check if '_c0' exists in the schema and drop it
+        if '_c0' in df.columns:
+            df = df.drop('_c0')
 
-    #Data for table
-    pandas_df.insert(0, "Sr No.", range(1, len(pandas_df) + 1))
 
-    # Prepare row data as list of lists
-    data_rows = pandas_df.values.tolist()  # This returns list of lists
+        # Receive parameters from the frontend
+        customer_id = request.GET.get('customer_id')
+        tx_month = request.GET.get('tx_month')
 
-    # Prepare data for pie chart
-    last_column = pandas_df.columns[-1]
-    pie_data = pandas_df[last_column].value_counts(normalize=True) * 100
-    pie_chart = {
-        "labels": pie_data.index.tolist(),
-        "values": pie_data.values.tolist()
-    }
+        # Convert TX_DATETIME to date and extract MM-YYYY
+        df = df.withColumn("TX_DATETIME", to_date(col("TX_DATETIME"), "yyyy-MM-dd HH:mm:ss"))
+        df = df.withColumn("TX_MONTH", date_format(col("TX_DATETIME"), "MM-yyyy"))
 
-    # Prepare data for bar chart
-    bar_data = pandas_df.groupby("CUSTOMER_ID")[last_column].value_counts(normalize=True).unstack(fill_value=0) * 100
-    bar_chart = {
-        "categories": bar_data.index.tolist(),
-        "series": [
-            {"name": col, "data": bar_data[col].tolist()} for col in bar_data.columns
-        ]
-    }
+        # Filter by customer_id and tx_month
+        if customer_id:
+            df = df.filter(col("CUSTOMER_ID") == int(customer_id))
+        if tx_month:
+            df = df.filter(col("TX_MONTH") == tx_month)
 
-    # Return JSON response
-    return JsonResponse({
-        "data": data_rows,
-        "pie_chart": pie_chart,
-        "bar_chart": bar_chart
-    })
+        # Convert to Pandas for DataTables
+        pandas_df = df.toPandas()
+
+        # Prepare data for pie chart
+        pie_data = pandas_df['TX_FRAUD'].value_counts(normalize=True) * 100
+        pie_chart = {
+            "labels": pie_data.index.tolist(),
+            "values": pie_data.values.tolist()
+        }
+        print("Pie chart data: ", pie_chart)
+
+
+        # Paginate the data for the table
+        draw = int(request.GET.get('draw', 1))
+        start = int(request.GET.get('start', 0))
+        length = int(request.GET.get('length', 10))
+        paginated_df = pandas_df.iloc[start:start + length].reset_index()
+        paginated_df['index'] = paginated_df['index'] + 1 + start
+
+        # Convert the paginated data to a list of lists
+        data = paginated_df.values.tolist()
+
+        # Return JSON response
+        return JsonResponse({
+            'draw': draw,
+            'recordsTotal': len(pandas_df),
+            'recordsFiltered': len(pandas_df),
+            'data': data,
+            'pie_chart': pie_chart
+        })
+    except Exception as e:
+        logger.error(f"Error in retrieve_data_by_id: {e}")
+        import traceback
+        logger.error(traceback.format_exc())
+        return JsonResponse({'error': str(e)}, status=500)
+
 
 def userLogout(request):
     try:
@@ -125,25 +291,22 @@ def userLogout(request):
     logout(request)
     return HttpResponseRedirect('/')
 
-def prediction_button(request, id):
-    data_file = get_object_or_404(DataFileUpload, pk=id)
-    file_path = data_file.actual_file.path  # Full path to uploaded file
+def predict_fraud(request):
+    if request.method == 'POST' and request.FILES.get('datafile'):
 
-    spark = SparkSession.builder \
-        .config("spark.sql.execution.arrow.pyspark.enabled", "true") \
-        .getOrCreate()
+        uploaded_file = request.FILES['datafile']
 
-    df = spark.read.csv(file_path, header=True, inferSchema=True)
+        spark = SparkSession.builder.getOrCreate()
 
-    pandas_df, bar_chart = run_prediction(df)
+        test_df = spark.read.csv(uploaded_file.temporary_file_path(), header=True, inferSchema=True)
 
-    return render(request, 'api/fraud_detection.html', {
-        'id': id,
-        'table_data': pandas_df.to_dict(orient="records"),
-        'columns': pandas_df.columns.tolist(),
-        'bar_chart': bar_chart
-    })
+        test_df = extract_time_features(test_df)
 
+        # trained_model = load_model('Website/ModelTraining/LogisticRegressionModel
+        loaded_pipeline_model, trained_rf_model = RandomForestClassifierWrapper.load_model_pip('Website/ModelTraining/RandomForestModel')
+        test_df = loaded_pipeline_model.transform(test_df)
+        predictions = trained_rf_model.transform(test_df)
+        #proba...
 def login2(request):
     data = {}
     if request.method == "POST":
